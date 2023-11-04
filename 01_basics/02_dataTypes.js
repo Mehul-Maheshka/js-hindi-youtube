bd8dfa050a1127000df31e45d3daee3b2c07a6b8
Mehul-Maheshka <123233758+Mehul-Maheshka@users.noreply.github.com>
2023-11-04 16:33:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict;" // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using node.js, not browser
+
+// code readability should be high
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power of 53
+// bright
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
