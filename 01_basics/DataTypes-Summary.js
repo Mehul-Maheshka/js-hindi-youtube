1af0fb0ede86a093994013c4ebcc2d24d7289561
Mehul-Maheshka <123233758+Mehul-Maheshka@users.noreply.github.com>
2023-11-08 06:07:49 +0000

    New changes stack and heap

--- 01_basics/DataTypes-Summary.js
+++ 01_basics/DataTypes-Summary.js
@@ -35,3 +35,28 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ***************----------******************
+
+// Stack (primitive), Heap (Non-Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
